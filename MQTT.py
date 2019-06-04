d37bbcd14dff2f511044b991891c4c588a399503
fb <florian.baeuerlein@gdts.one>
2019-06-04 16:55:40 +0200

    Some restructuring

---
+++ MQTT.py
@@ -0,0 +1,85 @@
+
+import paho.mqtt.client as MQTTClient
+import json
+import logging
+import sys
+
+logger = logging.getLogger("MQTT")
+logger.setLevel(logging.INFO)
+logger_handler = logging.StreamHandler(stream=sys.stdout)
+logger_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)-8s %(name)-12s %(message)s '))
+logger.addHandler(logger_handler)     
+
+class NamedBase(object):
+    def __init__(self, name):
+        self.name = name
+   
+
+class Publisher(NamedBase):
+    def __init__(self, topic : str):
+        self.pub_topic = topic + "/state"
+        super(Publisher, self).__init__(topic)
+        self.client = None
+
+    def get_value(self):
+        pass
+        #return { "value" : self.value }
+
+    def publish(self):
+        if self.client is None:
+            pass
+        self.client.publish(self.pub_topic, payload=json.dumps(self.get_value()))
+        self.report_state()
+    
+    def report_state(self):
+        logger.info("State: '{}'".format(self.get_value()))    
+
+class Subscriber(NamedBase):
+    def __init__(self, topic):
+        super(Subscriber, self).__init__(topic)
+        self.sub_topic = topic + "/set"
+
+    def set_value(self, value):
+        pass
+    
+    def on_message(self, client, userdata, message):
+        try:
+            msg = message.payload.decode('utf-8')
+            logger.info("received: '{}'".format(msg))
+            self.set_value(json.loads(msg)["value"])
+        except Exception as e:
+            logger.warning("Failed to decode/parse message: {}".format(e))
+            logger.warning('Message: {}'.format(msg))
+
+    def subscribe(self, client : MQTTClient.Client):
+        client.subscribe(self.sub_topic)
+        client.message_callback_add(self.sub_topic, self.on_message)    
+
+class Client(object):
+    def __init__(self):
+        self.client = MQTTClient.Client()
+        self.client.on_message = self.on_message
+        self.client.on_connect = self.on_connect
+        self.client.on_disconnect = self.on_disconnect
+        self.items = []
+
+    def connect(self, mqtt_host = "localhost"):
+        self.client.connect(mqtt_host)
+        self.client.loop_start()
+        logger.info("Connected to '{}'".format(mqtt_host))
+
+    def add(self, item):
+        item.client = self.client
+        self.items.append(item)
+
+    def on_connect(self, client, userdata, flags, rc):
+        for i in self.items:
+            # just actors can subscribe for changes
+            if isinstance(i, Subscriber):
+                i.subscribe(client)
+
+    def on_disconnect(self, client, userdata, rc):
+        logger.info("Disconnected!")
+
+    def on_message(self, client, userdata, message):
+        logger.info("Unhandled message received: {}".format(message.payload))
--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 import schedule
 import time
-import paho.mqtt.client as MQTTClient
 import json
 import logging
 import sys
 from functools import partial
-from SensorActor import Sensor, SensorActor, Actor
+from MQTT import Publisher, Subscriber, Client
 
-class Switch(SensorActor):
+class Switch(Publisher, Subscriber):
     '''
     Simple fake switch implementation
     '''
@@ -21,93 +20,31 @@ class Switch(SensorActor):
     def set_value(self, value):
         self.value = value
 
+class Temperature(Publisher):
+    def __init__(self, topic_base : str):
+        super(Temperature, self).__init__(topic_base)
+        self.value = 20
+
+    def get_value(self):
+        return self.value
+
 switch = Switch("/switch/fan/01")
-sensor = Sensor("/sensor/temperature/01")
-actor = Actor("/switch/fan/02")
-
-class Daemon(object):
-    def __init__(self):
-        self.client = MQTTClient.Client()
-        self.client.on_message = self.on_message
-        self.client.on_connect = self.on_connect
-        self.items = []
-
-    def connect(self, mqtt_host = "localhost"):
-        self.client.connect(mqtt_host)
-        self.client.loop_start()
-
-    def add(self, item):
-        item.client = self.client
-        self.items.append(item)
-
-    def on_connect(self, client, userdata, flags, rc):
-        for i in self.items:
-            # just actors can subscribe for changes
-            if isinstance(i, Actor):
-                i.subscribe(client)
-
-    def on_disconnect(self, client, userdata, rc):
-        print("Disconnected!")
-
-    def on_message(self, client, userdata, message):
-        print("Unhandled message received: {}".format(message.payload))
+sensor = Temperature("/sensor/temperature/01")
+# actor = Actor("/switch/fan/02")
 
 
-daemon = Daemon()
-daemon.connect("localhost")
-daemon.add(switch)
-daemon.add(sensor)
-daemon.add(actor)
+mqtt_client = Client()
+
+# first add sensors and actors
+mqtt_client.add(switch)
+mqtt_client.add(sensor)
+
+# finally connect
+mqtt_client.connect("localhost")
 
 schedule.every(5).seconds.do(switch.publish)
-schedule.every(15).seconds.do(sensor.publish)
+schedule.every(10).seconds.do(sensor.publish)
 
 while True:
     schedule.run_pending()
     time.sleep(5)
-
-
-#class Sensor(object):
-#     def __init__(self, topic : str, default_value = None):
-#         self.pub_topic = topic + "/state"
-#         self.value = default_value
-
-#         self.logger = logging.getLogger(topic)
-#         self.logger.setLevel(logging.INFO)
-#         self.logger_handler = logging.StreamHandler(stream=sys.stdout)
-#         self.logger_handler.setFormatter(logging.Formatter('%(asctime)s %(name)-12s %(levelname)-8s %(message)s '))
-#         self.logger.addHandler(self.logger_handler)
-#         #self.logger.info("Setting up sensor: '{}' '{}'".format(self.pub_topic, self.sub_topic))        
-
-#     def get_value(self):
-#         return { "value" : self.value }
-
-#     def publish(self, client):
-#         client.publish(self.pub_topic, payload=json.dumps(self.get_value()))
-    
-#     def report_state(self):
-#         self.logger.info("State: '{}'".format(self.get_value()))    
-
-# class Actor(object):
-#     def __init__(self, topic_base : str, default_value = None):
-#         super(Switch, self).__init__(topic_base, default_value=default_value)
-#         self.sub_topic = topic_base + "/set"
-
-# class Switch(Sensor):
-#     def __init__(self, topic_base : str, default_value = None):
-#         super(Switch, self).__init__(topic_base, default_value=default_value)
-#         self.sub_topic = topic_base + "/set"
-
-#     def on_message(self, client, userdata, message):
-#         try:
-#             msg = message.payload.decode('utf-8')
-#             self.value = json.loads(msg)["value"]
-#         except Exception as e:
-#             self.logger.warning("Failed to parse message: {}".format(e))
-#             self.logger.warning('Message: {}'.format(msg))
-
-#     def subscribe(self, client):
-#         client.subscribe(self.sub_topic)
-#         client.message_callback_add(self.sub_topic, self.on_message)
-
-
